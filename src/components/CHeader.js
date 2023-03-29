1f37465017a14b0c08875bd9cdd8d8b9f4e95b2f
Krysmal <malinowskizxcvbnm@gmail.com>
2023-03-29 20:09:40 +0200

    commit test 1

---
+++ src/components/CHeader.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import "./CHeader.css";
+
+class CHeader extends Component
+{
+    render()
+    {
+        return(
+            <div className="header">
+                <div className="title">MED</div>
+                
+            </div>
+        )
+    }
+
+}
+
+export default CHeader;
