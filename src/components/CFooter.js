bfdf6a4941830b968c3f6ed15452913b42dc937f
Krysmal <malinowskizxcvbnm@gmail.com>
2023-04-16 11:42:39 +0200

    footer

--- src/components/CFooter.js
+++ src/components/CFooter.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import "./CFooter.css";
 import logo from "../img/logo.svg";
-
+import logo_yt from "../img/logo_yt.png";
+import logo_fb from "../img/logo_fb.png";
 class CHeader extends Component
 {
     render()
@@ -10,12 +11,15 @@ class CHeader extends Component
             <div className="footer">
                 <div className="footer_comp">
                     <div className="footer_comp_header"><b>Zobacz Także</b></div>
-                    <div className="footer_comp_item"></div>
+                    <div className="footer_comp_social_media">
+                        <a className="footer_social_media_link" href="https://www.youtube.com" target="_blank" rel="noreferrer"><img className="footer_social_media_logo" src={logo_yt} alt="logo_yt"></img></a>
+                        <a className="footer_social_media_link" href="https://www.facebook.com" target="_blank" rel="noreferrer"><img className="footer_social_media_logo" src={logo_fb} alt="logo_yt"></img></a>
+                    </div>
                 </div>
                 <div className="footer_comp">
                     <div className="footer_comp_header"><b>Użyteczne linki</b></div>
-                    <div className="footer_comp_item"><a href="https://www.nfz.gov.pl">NFZ</a></div>
-                    <div className="footer_comp_item"><a href="https://www.gov.pl/web/zdrowie">Ministerstwo Zdrowia</a></div>
+                    <div className="footer_comp_item"><a className="footer_link" href="https://www.nfz.gov.pl">NFZ</a></div>
+                    <div className="footer_comp_item"><a className="footer_link" href="https://www.gov.pl/web/zdrowie">Ministerstwo Zdrowia</a></div>
 
                 </div>
                 <div className="footer_comp">
